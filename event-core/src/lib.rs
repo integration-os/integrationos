613d2bbd7c362ee0dcfaf533bc2ca22f007ac17e
Samuel Gomez <samgj_18@hotmail.com>
2024-05-21 21:05:49 +0100

    Renaming event-core -> integrationos-event

--- event-core/src/lib.rs
+++
@@ -1,8 +0,0 @@
-pub mod config;
-pub mod dispatcher;
-pub mod event_handler;
-pub mod metrics;
-pub mod mock;
-pub mod mongo_context_store;
-pub mod mongo_control_data_store;
-pub mod store;
--- event-core/src/mock/mod.rs
+++
@@ -1,2 +0,0 @@
-pub mod mock_context_store;
-pub mod mock_secret_service;
