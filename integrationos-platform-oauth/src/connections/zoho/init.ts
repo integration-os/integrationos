b2b5d7be805434ff649c397d3a7e189a51e4d7cb
Gaurav  Oberoi <155448028+oberoi-gaurav@users.noreply.github.com>
2024-06-19 23:14:40 +0530

    feat: added zoho oauth endpoints (#72)

---
+++ integrationos-platform-oauth/src/connections/zoho/init.ts
@@ -0,0 +1,44 @@
+import axios from "axios";
+import { DataObject, OAuthResponse } from "../../lib/types";
+
+export const init = async ({ body }: DataObject): Promise<OAuthResponse> => {
+  try {
+    const {
+      clientId,
+      clientSecret,
+      metadata: {
+        additionalData,
+        code,
+        formData: { ZOHO_ACCOUNTS_DOMAIN },
+        redirectUri,
+      },
+    } = body;
+    let url = `${ZOHO_ACCOUNTS_DOMAIN}/oauth/v2/token?grant_type=authorization_code`;
+    url += `&client_id=${clientId}&client_secret=${clientSecret}&code=${code}&redirect_uri=${redirectUri}`;
+
+    const response = await axios.post(url);
+
+    const {
+      data: {
+        access_token: accessToken,
+        refresh_token: refreshToken,
+        api_domain: apiDomain,
+        token_type: tokenType,
+        expires_in: expiresIn,
+      },
+    } = response;
+
+    return {
+      accessToken,
+      refreshToken,
+      expiresIn,
+      tokenType,
+      meta: {
+        apiDomain,
+        ...additionalData,
+      },
+    };
+  } catch (error) {
+    throw new Error(`Error fetching access token for Zoho: ${error}`);
+  }
+};
