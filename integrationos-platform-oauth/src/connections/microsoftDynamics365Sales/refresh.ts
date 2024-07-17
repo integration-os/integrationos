fc2bdb41eb1ac3c94c2a7de5428ba4a4f868b76f
Gaurav  Oberoi <155448028+oberoi-gaurav@users.noreply.github.com>
2024-07-17 19:55:20 +0530

    feat: fixed refresh token issue in Microsoft Sales (#92)

--- integrationos-platform-oauth/src/connections/microsoftDynamics365Sales/refresh.ts
+++ integrationos-platform-oauth/src/connections/microsoftDynamics365Sales/refresh.ts
@@ -3,40 +3,43 @@ import qs from "qs";
 import { DataObject, OAuthResponse } from "../../lib/types";
 
 export const refresh = async ({ body }: DataObject): Promise<OAuthResponse> => {
-	try {
-		const requestBody = {
-			grant_type: "client_credentials",
-			client_id: body.clientId,
-			client_secret: body.clientSecret,
-			resource: body.metadata?.formData?.MICROSOFT_DYNAMICS_365_SALES_ORGANIZATION_URI,
-		};
+  try {
+    const {
+      OAUTH_CLIENT_ID: client_id,
+      OAUTH_CLIENT_SECRET: client_secret,
+      OAUTH_REQUEST_PAYLOAD: { formData },
+      OAUTH_METADATA,
+    } = body;
+    const requestBody = {
+      grant_type: "client_credentials",
+      client_id,
+      client_secret,
+      resource: formData?.MICROSOFT_DYNAMICS_365_SALES_ORGANIZATION_URI,
+    };
 
-		const response = await axios({
-			url: `https://login.windows.net/${body.metadata?.formData?.MICROSOFT_DYNAMICS_365_SALES_TENANT_ID}/oauth2/token`,
-			method: "POST",
-			headers: { "content-type": "application/x-www-form-urlencoded" },
-			data: qs.stringify(requestBody)
-		});
+    const response = await axios({
+      url: `https://login.windows.net/${formData?.MICROSOFT_DYNAMICS_365_SALES_TENANT_ID}/oauth2/token`,
+      method: "POST",
+      headers: { "content-type": "application/x-www-form-urlencoded" },
+      data: qs.stringify(requestBody),
+    });
 
-		const {
-			data: {
-				access_token,
-				expires_in,
-				token_type,
-				resource,
-			}
-		} = response;
+    const {
+      data: { access_token, expires_in, token_type },
+    } = response;
 
-		return {
-			accessToken: access_token,
-			refreshToken: "",
-			expiresIn: +expires_in,
-			tokenType: token_type,
-			meta: {
-				resource,
-			}
-		};
-	} catch (error) {
-		throw new Error(`Error fetching refresh token for Microsoft Dynamics 365 Sales: ${error}`);
-	}
+    return {
+      accessToken: access_token,
+      refreshToken: "",
+      expiresIn: +expires_in,
+      tokenType: token_type,
+      meta: {
+        ...OAUTH_METADATA?.meta,
+      },
+    };
+  } catch (error) {
+    throw new Error(
+      `Error fetching refresh token for Microsoft Dynamics 365 Sales: ${error}`
+    );
+  }
 };
