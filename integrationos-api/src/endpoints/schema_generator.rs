2622ac0f46975705ed0a112c2dedff2946a33663
Samuel Gomez <samgj_18@hotmail.com>
2024-07-24 15:41:03 +0100

    Adding typescript type endpoint and fixing ts types

--- integrationos-api/src/endpoints/schema_generator.rs
+++ integrationos-api/src/endpoints/schema_generator.rs
@@ -7,7 +7,9 @@ use axum::{
 };
 use bson::{doc, Document};
 use futures::StreamExt;
-use integrationos_domain::{ApplicationError, Id, IntegrationOSError, InternalError, Store};
+use integrationos_domain::{
+    api_model_config::Lang, ApplicationError, Id, IntegrationOSError, InternalError, Store,
+};
 use mongodb::options::FindOptions;
 use std::sync::Arc;
 
@@ -15,6 +17,7 @@ pub fn get_router() -> Router<Arc<AppState>> {
     Router::new()
         .route("/projection", get(get_common_model_proj))
         .route("/:id", get(generate_schema))
+        .route("/types/:id", get(generate_types))
 }
 
 pub async fn get_common_model_proj(
@@ -59,6 +62,29 @@ pub async fn get_common_model_proj(
         skip: 0,
         limit: 0,
     }))
+}
+
+pub async fn generate_types(
+    state: State<Arc<AppState>>,
+    Path(id): Path<Id>,
+) -> Result<String, IntegrationOSError> {
+    let cm_store = state.app_stores.common_model.clone();
+    let ce_store = state.app_stores.common_enum.clone();
+
+    let common_model = cm_store
+        .get_one_by_id(&id.to_string())
+        .await
+        .map_err(IntegrationOSError::from)?
+        .ok_or(ApplicationError::not_found(
+            &format!("CommonModel with id {} not found", id),
+            None,
+        ))?;
+
+    let schema = common_model
+        .generate_as_expanded(&Lang::TypeScript, &cm_store, &ce_store)
+        .await;
+
+    Ok(schema)
 }
 
 pub async fn generate_schema(
