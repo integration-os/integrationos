bc30b2bab7ffb121a5261e481e44e5ab72192913
Samuel Gomez <samgj_18@hotmail.com>
2024-08-09 18:41:28 +0100

    enable query parameters to request

---
+++ integrationos-api/tests/api_tests/connection_tests.rs
@@ -0,0 +1,13 @@
+use crate::test_server::TestServer;
+use http::{Method, StatusCode};
+use serde_json::Value;
+
+#[tokio::test]
+async fn test_connection_data_models_api() {
+    let server = TestServer::new(None).await;
+    let res = server
+        .send_request::<Value, Value>("v1/public/connection-data", Method::GET, None, None)
+        .await
+        .unwrap();
+    assert_eq!(res.code, StatusCode::OK);
+}
