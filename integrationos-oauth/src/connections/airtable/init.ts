8c48d41ca53cab9dc4b88d179a65ffd0b07a01b9
Siddharth Bhansali <siddharthbhansali13@hotmail.com>
2024-08-28 19:07:05 +0530

    Airtable - Fixing Formatting

--- integrationos-oauth/src/connections/airtable/init.ts
+++ integrationos-oauth/src/connections/airtable/init.ts
@@ -1,5 +1,4 @@
 import axios from "axios";
-
 import { DataObject, OAuthResponse } from "../../lib/types";
 import qs from "qs";
 
@@ -18,52 +17,51 @@ export const base64UrlEncode = async (input: string) => {
 };
 
 export const init = async ({ body }: DataObject): Promise<OAuthResponse> => {
-		try {
-			const requestBody = {
-				grant_type: "authorization_code",
-				code: body.metadata?.code,
-				client_id: body.clientId,
-				client_secret: body.clientSecret,
-				redirect_uri: body.metadata?.redirectUri,
-				code_verifier: "eNmtK0mRHQ.-93QxgrniB7rb.-TZ_Tjbonygt2aqk1.ltiwXQHmNFeFJPh19MZwXzFDfmFMpUZbAFVtSAtChNU08R4txdDBi7EY6ZHiBp6I8F1drUHZR",
-			};
+	try {
+		const requestBody = {
+			grant_type: "authorization_code",
+			code: body.metadata?.code,
+			client_id: body.clientId,
+			client_secret: body.clientSecret,
+			redirect_uri: body.metadata?.redirectUri,
+			code_verifier: "eNmtK0mRHQ.-93QxgrniB7rb.-TZ_Tjbonygt2aqk1.ltiwXQHmNFeFJPh19MZwXzFDfmFMpUZbAFVtSAtChNU08R4txdDBi7EY6ZHiBp6I8F1drUHZR",
+		};
 
-			const authorizationToken = await base64UrlEncode(`${body.clientId}:${body.clientSecret}`);
+		const authorizationToken = await base64UrlEncode(`${body.clientId}:${body.clientSecret}`);
 
-			const response = await axios({
-				method: "POST",
-				headers: {
-					"Content-Type": "application/x-www-form-urlencoded",
-					"Authorization": `Basic ${authorizationToken}`
-				},
-				data: qs.stringify(requestBody),
-				url: "https://airtable.com/oauth2/v1/token",
-			});
+		const response = await axios({
+			method: "POST",
+			headers: {
+				"Content-Type": "application/x-www-form-urlencoded",
+				"Authorization": `Basic ${authorizationToken}`
+			},
+			data: qs.stringify(requestBody),
+			url: "https://airtable.com/oauth2/v1/token",
+		});
 
-			const {
-				data: {
-					token_type: tokenType,
-					scope,
-					access_token: accessToken,
-					expires_in: expiresIn,
-					refresh_token: refreshToken,
-					refresh_expires_in: refreshExpiresIn,
-				}
-			} = response;
+		const {
+			data: {
+				token_type: tokenType,
+				scope,
+				access_token: accessToken,
+				expires_in: expiresIn,
+				refresh_token: refreshToken,
+				refresh_expires_in: refreshExpiresIn,
+			}
+		} = response;
 
-			return {
-				accessToken,
-				refreshToken,
-				expiresIn,
-				tokenType,
-				meta: {
-					scope,
-					refreshExpiresIn
-				}
-			};
-		} catch
-			(error) {
-			throw new Error(`Error fetching access token for Airtable: ${error}`);
-		}
+		return {
+			accessToken,
+			refreshToken,
+			expiresIn,
+			tokenType,
+			meta: {
+				scope,
+				refreshExpiresIn
+			}
+		};
+	} catch
+		(error) {
+		throw new Error(`Error fetching access token for Airtable: ${error}`);
 	}
-;
+};
--- integrationos-oauth/src/connections/airtable/refresh.ts
+++ integrationos-oauth/src/connections/airtable/refresh.ts
@@ -1,5 +1,4 @@
 import axios from "axios";
-
 import { DataObject, OAuthResponse } from "../../lib/types";
 import qs from "qs";
 
@@ -18,54 +17,53 @@ export const base64UrlEncode = async (input: string) => {
 };
 
 export const refresh = async ({ body }: DataObject): Promise<OAuthResponse> => {
-		try {
-			const {
-				OAUTH_CLIENT_ID: clientId,
-				OAUTH_CLIENT_SECRET: clientSecret,
-				OAUTH_REFRESH_TOKEN: refresh_token
-			} = body;
+	try {
+		const {
+			OAUTH_CLIENT_ID: clientId,
+			OAUTH_CLIENT_SECRET: clientSecret,
+			OAUTH_REFRESH_TOKEN: refresh_token
+		} = body;
 
-			const requestBody = {
-				grant_type: "refresh_token",
-				refresh_token,
-			};
+		const requestBody = {
+			grant_type: "refresh_token",
+			refresh_token,
+		};
 
-			const authorizationToken = await base64UrlEncode(`${clientId}:${clientSecret}`);
+		const authorizationToken = await base64UrlEncode(`${clientId}:${clientSecret}`);
 
-			const response = await axios({
-				method: "POST",
-				headers: {
-					"Content-Type": "application/x-www-form-urlencoded",
-					"Authorization": `Basic ${authorizationToken}`
-				},
-				data: qs.stringify(requestBody),
-				url: "https://airtable.com/oauth2/v1/token",
-			});
+		const response = await axios({
+			method: "POST",
+			headers: {
+				"Content-Type": "application/x-www-form-urlencoded",
+				"Authorization": `Basic ${authorizationToken}`
+			},
+			data: qs.stringify(requestBody),
+			url: "https://airtable.com/oauth2/v1/token",
+		});
 
-			const {
-				data: {
-					token_type: tokenType,
-					scope,
-					access_token: accessToken,
-					expires_in: expiresIn,
-					refresh_token: refreshToken,
-					refresh_expires_in: refreshExpiresIn,
-				}
-			} = response;
+		const {
+			data: {
+				token_type: tokenType,
+				scope,
+				access_token: accessToken,
+				expires_in: expiresIn,
+				refresh_token: refreshToken,
+				refresh_expires_in: refreshExpiresIn,
+			}
+		} = response;
 
-			return {
-				accessToken,
-				refreshToken,
-				expiresIn,
-				tokenType,
-				meta: {
-					scope,
-					refreshExpiresIn
-				}
-			};
-		} catch
-			(error) {
-			throw new Error(`Error fetching refresh token for Airtable: ${error}`);
-		}
+		return {
+			accessToken,
+			refreshToken,
+			expiresIn,
+			tokenType,
+			meta: {
+				scope,
+				refreshExpiresIn
+			}
+		};
+	} catch
+		(error) {
+		throw new Error(`Error fetching refresh token for Airtable: ${error}`);
 	}
-;
+};
