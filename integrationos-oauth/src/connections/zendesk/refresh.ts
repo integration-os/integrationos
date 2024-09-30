82b4340b11794df42abbc70d9970129d539c0547
Gaurav  Oberoi <155448028+oberoi-gaurav@users.noreply.github.com>
2024-10-01 01:37:24 +0530

    feat: added zendesk oauth endpoints (#166)

---
+++ integrationos-oauth/src/connections/zendesk/refresh.ts
@@ -0,0 +1,18 @@
+import { DataObject, OAuthResponse } from '../../lib/types';
+
+export const refresh = async ({ body }: DataObject): Promise<OAuthResponse> => {
+    try {
+        const {
+            OAUTH_METADATA: { accessToken, refreshToken, tokenType, meta },
+        } = body;
+        return {
+            accessToken,
+            refreshToken,
+            expiresIn: 2147483647,
+            tokenType,
+            meta,
+        };
+    } catch (error) {
+        throw new Error(`Error fetching refresh token for Zendesk: ${error}`);
+    }
+};
