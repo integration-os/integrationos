094b919a0fab6dd3e0a9f7d66b016b06f0e0dcab
Samuel <39674930+sagoez@users.noreply.github.com>
2024-10-17 15:05:38 +0100

    fix: using connection manager for a persistent redis connection (#177)

--- integrationos-domain/src/domain/configuration/cache.rs
+++ integrationos-domain/src/domain/configuration/cache.rs
@@ -13,12 +13,12 @@ pub struct CacheConfig {
     pub api_throughput_key: String,
     #[envconfig(from = "REDIS_POOL_SIZE", default = "10")]
     pub pool_size: usize,
-    #[envconfig(env = "CACHE_WAIT_TIMEOUT_SECONDS", default = "30")]
-    pub wait_timeout: u64,
-    #[envconfig(env = "CACHE_CREATE_TIMEOUT_SECONDS", default = "30")]
-    pub create_timeout: u64,
-    #[envconfig(env = "CACHE_RECYCLE_TIMEOUT_SECONDS", default = "30")]
-    pub recycle_timeout: u64,
+    #[envconfig(env = "CACHE_MAX_DELAY_SECONDS", default = "30")]
+    pub max_delay: u64,
+    #[envconfig(env = "CACHE_RESPONSE_TIMEOUT_SECONDS", default = "30")]
+    pub response_timeout: u64,
+    #[envconfig(env = "CACHE_CONNECTION_TIMEOUT_SECONDS", default = "30")]
+    pub connection_timeout: u64,
 }
 
 impl Default for CacheConfig {
@@ -29,9 +29,9 @@ impl Default for CacheConfig {
             event_throughput_key: "event_throughput".to_owned(),
             api_throughput_key: "api_throughput".to_owned(),
             pool_size: 10,
-            wait_timeout: 30,
-            create_timeout: 30,
-            recycle_timeout: 30,
+            max_delay: 30,
+            response_timeout: 30,
+            connection_timeout: 30,
         }
     }
 }
@@ -47,8 +47,12 @@ impl Display for CacheConfig {
         )?;
         writeln!(f, "REDIS_API_THROUGHPUT_KEY: {}", self.api_throughput_key)?;
         writeln!(f, "REDIS_POOL_SIZE: {}", self.pool_size)?;
-        writeln!(f, "CACHE_WAIT_TIMEOUT_SECONDS: {}", self.wait_timeout)?;
-        writeln!(f, "CACHE_CREATE_TIMEOUT_SECONDS: {}", self.create_timeout)?;
-        writeln!(f, "CACHE_RECYCLE_TIMEOUT_SECONDS: {}", self.recycle_timeout)
+        writeln!(f, "CACHE_WAIT_TIMEOUT_SECONDS: {}", self.max_delay)?;
+        writeln!(f, "CACHE_CREATE_TIMEOUT_SECONDS: {}", self.response_timeout)?;
+        writeln!(
+            f,
+            "CACHE_RECYCLE_TIMEOUT_SECONDS: {}",
+            self.connection_timeout
+        )
     }
 }
