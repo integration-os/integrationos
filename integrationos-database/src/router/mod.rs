50a38eb09cc2300f586052d0a943722971b3b47f
Samuel Gomez <samgj_18@hotmail.com>
2024-10-23 18:33:01 +0100

    adding trace layer to database pod

--- integrationos-database/src/router/mod.rs
+++ integrationos-database/src/router/mod.rs
@@ -1,9 +1,10 @@
 use crate::{logic::connection, server::AppState};
-use axum::{response::IntoResponse, routing::get, Json, Router};
+use axum::{middleware::from_fn, response::IntoResponse, routing::get, Json, Router};
 use http::StatusCode;
+use integrationos_domain::telemetry::log_request_middleware;
 use serde_json::json;
 use std::sync::Arc;
-use tower_http::cors::CorsLayer;
+use tower_http::{cors::CorsLayer, trace::TraceLayer};
 
 pub async fn get_router() -> Router<Arc<AppState>> {
     Router::new()
@@ -11,6 +12,8 @@ pub async fn get_router() -> Router<Arc<AppState>> {
         .route("/", get(get_root))
         .fallback(not_found_handler)
         .layer(CorsLayer::permissive())
+        .layer(from_fn(log_request_middleware))
+        .layer(TraceLayer::new_for_http())
 }
 
 pub async fn get_root() -> impl IntoResponse {
