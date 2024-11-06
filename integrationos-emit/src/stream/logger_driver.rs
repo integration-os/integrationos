5ffb3015eeb68ade143d660e8c742aac2a165a28
Samuel Gomez <samgj_18@hotmail.com>
2024-11-06 19:03:06 +0000

    chore: adding Dockerfile and github workflow

---
+++ integrationos-emit/src/stream/logger_driver.rs
@@ -0,0 +1,44 @@
+use super::{EventStreamExt, EventStreamTopic};
+use crate::{domain::event::EventEntity, server::AppState};
+use async_trait::async_trait;
+use fluvio::consumer::Record;
+use integrationos_domain::{prefix::IdPrefix, Id, IntegrationOSError, Unit};
+use std::boxed::Box;
+use tokio_util::sync::CancellationToken;
+
+pub struct LoggerDriverImpl;
+
+#[async_trait]
+impl EventStreamExt for LoggerDriverImpl {
+    async fn publish(
+        &self,
+        event: EventEntity,
+        _target: EventStreamTopic,
+    ) -> Result<Id, IntegrationOSError> {
+        tracing::info!("Received event: {:?}, using logger handler", event);
+
+        Ok(Id::now(IdPrefix::PipelineEvent))
+    }
+
+    async fn consume(
+        &self,
+        _token: CancellationToken,
+        _target: EventStreamTopic,
+        _ctx: &AppState,
+    ) -> Result<Unit, IntegrationOSError> {
+        tracing::info!("Consuming events using logger handler");
+
+        Ok(())
+    }
+
+    async fn process(
+        &self,
+        _ctx: &AppState,
+        _target: EventStreamTopic,
+        _event: &Record,
+    ) -> Result<Unit, IntegrationOSError> {
+        tracing::info!("Processing events using logger handler");
+
+        Ok(())
+    }
+}
